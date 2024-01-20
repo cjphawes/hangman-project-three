83217537504cb9ff2aee4b631e4daab73a08fa72
cjphawes <cameronhawes98@gmail.com>
2024-01-20 18:52:15 +0000

    feat:add if else statement to start_game function

--- run.py
+++ run.py
@@ -3,11 +3,18 @@ import random
 
 
 def start_game():
-        try:
-            start = input("Would you like to start the game? Y/N: ")
-            print(f"You chose {start}")
-        except ValueError:
-            print("I didn't ask for that did I?")
+    user_response = input("Would you like to start the game? Y/N: \n")
+    if user_response == "Y" or "y":
+        print("Entering Game Now...\n")
+    elif user_response == "N" or"n":
+        print("Exiting Game Now...\n")
+    else:
+        print("Invalid input, please enter Y or N.")
+
+    # try:
+    #     print(f"You chose {user_response}")
+    # except ValueError:
+    #     print("I didn't ask for that did I?")
 
 
 
