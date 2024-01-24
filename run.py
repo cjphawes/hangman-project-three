1e9a80a62e78f3e917c1e0c5cf2830aff593f9b6
cjphawes <cameronhawes98@gmail.com>
2024-01-24 16:01:42 +0000

    feat:add emoji's for lives and a green tick to rules function

--- run.py
+++ run.py
@@ -31,17 +31,20 @@ def display_rules():
     Displays the rules for the game
     """
     heart_emoji = emoji.emojize("\u2764\ufe0f")
+    heart_emoji_8 = heart_emoji * 8
+    green_tick = emoji.emojize("\u2705")
+    
 
     rules = (
         f"\n\nHere are the rules:\n\n"
         f"1. Insert your username.\n"
         f"2. I will give you a word, it is your job to guess the letters\n"
         f"   that make up that word.\n" 
-        f"3. You have {heart_emoji}  Lives available, every time you get a\n"
-        f"   letter incorrect, you will lose a life."
+        f"3. You have 8 {heart_emoji_8}  lives available, every time you get\n"
+        f"   a letter incorrect, you will lose a {heart_emoji}\n"
         f"4. Guess a letter and I will tell you if it is correct or not.\n"
-        f"5. Keep guessing until you either, lose all your lives or you\n"
-        f"   guess the word correctly.\n"
+        f"5. Keep guessing until you either, lose all your {heart_emoji} 's\n"
+        f"   or you guess the word correctly.{green_tick}\n"
         f"6. Finally, have fun!\n"
         )
     print(rules)
@@ -76,9 +79,9 @@ def get_correct_word(list_of_words):
         
     return new_word.upper()
 
-# def validate_letter():
-#     new_word = get_correct_word(list_of_words)
-#     word_makeup = set(new_word)
+def validate_letter():
+    new_word = get_correct_word(list_of_words)
+    word_makeup = set(new_word)
 
 
 def main():
@@ -87,6 +90,7 @@ def main():
     user = input_username()
     display_word = get_correct_word(list_of_words)
     print(f"{user} your word is {display_word}")
+    validate_letter()
 
 
 main()
