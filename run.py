9d079b3fe380a7a306df984f0063610f12e11270
cjphawes <cameronhawes98@gmail.com>
2024-01-25 15:29:37 +0000

    feat:add function guess_word, import string module, add emojis

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import random
 from words import list_of_words
 import emoji
+import string
 
 
 
@@ -31,7 +32,7 @@ def display_rules():
     Displays the rules for the game
     """
     heart_emoji = emoji.emojize("\u2764\ufe0f")
-    heart_emoji_8 = heart_emoji * 8
+    heart_emoji_multiplied = heart_emoji * 8
     green_tick = emoji.emojize("\u2705")
     
 
@@ -40,8 +41,8 @@ def display_rules():
         f"1. Insert your username.\n"
         f"2. I will give you a word, it is your job to guess the letters\n"
         f"   that make up that word.\n" 
-        f"3. You have 8 {heart_emoji_8}  lives available, every time you get\n"
-        f"   a letter incorrect, you will lose a {heart_emoji}\n"
+        f"3. You have 8 {heart_emoji_multiplied}  lives available, every\n"
+        f"   time you get a letter incorrect, you will lose a {heart_emoji}\n"
         f"4. Guess a letter and I will tell you if it is correct or not.\n"
         f"5. Keep guessing until you either, lose all your {heart_emoji} 's\n"
         f"   or you guess the word correctly.{green_tick}\n"
@@ -63,7 +64,7 @@ def input_username():
         else:
             print(
                 f"{username} is not a valid username, please use one without\n"
-                f"numbers or special characters"
+                f"numbers or special characters."
                 )
     return username
 
@@ -79,9 +80,47 @@ def get_correct_word(list_of_words):
         
     return new_word.upper()
 
-def validate_letter():
+def guess_word():
+    """
+    Validates the letter chosen if it is either in or not in the word that is
+    being guessed by the user
+    """
     new_word = get_correct_word(list_of_words)
-    word_makeup = set(new_word)
+    # lives_allowed = 8
+    word_makeup = set(new_word) #tell us the letters that make up the word
+    alphabet = set(string.ascii_uppercase) #letters of alphabet in CAPS
+    guessed_letters = set() #this tells us what the user has guessed
+
+    """
+    Grabbing the users input for a letter guess and seeing if it is a letter
+    they have guessed already or not
+    """
+    while len(word_makeup) > 0:
+        letter_guess = input("Guess a letter: ").upper()
+        print("")
+
+        if letter_guess in alphabet - guessed_letters:
+            guessed_letters.add(letter_guess)
+
+            if letter_guess in word_makeup:
+                word_makeup.remove(letter_guess)
+        
+        elif letter_guess in guessed_letters:
+            print("Whoops! You cannot guess the same letter twice.\n")
+
+        else:
+            print("Invalid character, please try again.\n")
+
+        #telling the user what letters they have guessed
+        print("You have used letters: ", ' '.join(guessed_letters))
+        
+        #what the current word is being guessed
+        word_list = [letter if letter in guessed_letters else '-' for letter in new_word]
+        print("The current word is: ", ' '.join(word_list))
+        print("")
+
+    return word_makeup
+
 
 
 def main():
@@ -89,8 +128,6 @@ def main():
     display_rules()
     user = input_username()
     display_word = get_correct_word(list_of_words)
-    print(f"{user} your word is {display_word}")
-    validate_letter()
-
+    guess_word()
 
 main()
