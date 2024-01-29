a5e802dec897fbd4c31602aebef005fe5f0757b2
cjphawes <cameronhawes98@gmail.com>
2024-01-29 20:51:17 +0000

    feat:change f-strings, use import *, add new rules

--- run.py
+++ run.py
@@ -1,24 +1,13 @@
+#
 import random
 import os
 from words import list_of_words
 import string
 from colorama import Fore, Style
-from number_styling import (underline,
-color_white, 
-color_green,
-color_red,
-styling_end)
-from emoji_dict import (waving_hand,
-confused_emoji,
-heart_emoji,
-heart_emoji_multiplied,
-green_tick,
-writing,
-prohibited,
-cross_emoji,
-shocked_face,
-face_with_hearts,
-winky_face)
+
+#I understand concern with using * however i know i won't reuse names, put in README
+from number_styling import *
+from emoji_dict import *
 
 def start_game():
     """
@@ -30,18 +19,24 @@ def start_game():
     lines = file.read()
     file.close()
     print(lines)
-    print("\n\nThe exciting word-guessing game where your vocabulary skills"
-        " are put to the test! In this game, you have a limited number of lives"
-        " to guess the hidden word correctly. With each incorrect guess, you'll"
-        " lose a life. Can you figure out the word and avoid running out of"
-        " lives? Challenge yourself and see how many words you can solve in a"
-        " row! Let's dive in and start guessing!\n\n")
+    print(
+        f"\n\nThe exciting word-guessing game where your vocabulary skills"
+        f" are put to the test! In this game, you have a limited number"
+        f" of {heart_emoji}  lives to guess the hidden word correctly. With" 
+        f" each incorrect guess, you'll lose a {heart_emoji}  life. Can"
+        f" {color_white}YOU{styling_end} figure out the word and avoid running"
+        f" out of lives? {color_white}Challenge{styling_end} yourself and see"
+        f" how many words you can solve in a row! Let's dive in and start"
+        f" guessing!\n\n"
+        )
 
 def enter_game():
     # Asks user if they are ready to start the game, with user validation
     while True:
-        user_response = input(f"Would you like to start the game? {color_green}"
-            f"Y{styling_end}/{color_red}N{styling_end}: ").upper()
+        user_response = input(
+            f"Would you like to start the game? {color_green}Y{styling_end}/"
+            f"{color_red}N{styling_end}: "
+            ).upper()
 
         if user_response == "Y":
             print("Entering Game Now...\n")
@@ -50,15 +45,16 @@ def enter_game():
             print(f"Exiting Game Now...\nThank you for playing!{waving_hand}")
             exit()
         else:
-            print(f"""{confused_emoji} I didn't ask for that did I?, please 
-                enter Y or N.\n""")
+            print(
+                f"{confused_emoji} I didn't ask for that did I?, please"
+                f" enter Y or N.\n"
+                )
     return user_response
 
 def display_rules():
-    """
-    Displays the rules for the game
-    """
-    rules = (f"\n\n{underline}Here are the rules{styling_end} {writing}\n\n"
+    # Displays the rules for the game
+    rules = (
+        f"\n{underline}Here are the rules{styling_end} {writing}\n\n"
         f"{color_white}1.{styling_end} I will give you a mystery word, it is"
         f" your job to guess the letters that make up that word.\n{color_white}"
         f"2.{styling_end} You have 8 {heart_emoji_multiplied}  lives available,"
@@ -68,9 +64,13 @@ def display_rules():
         f" until you either, lose all your {heart_emoji} 's, or you guess the"
         f" word correctly {green_tick}\n{color_white}6.{styling_end} You will"
         f" not know the length of the word until you make your first guess!"
-        f" Mysterious I know {winky_face} It could be 4 letters or upto 9!\n"
-        f"{color_white}5.{styling_end} Finally, insert your username and"
-        f"have fun!\n")
+        f" Mysterious I know {winky_face}  It could be from 4-9 letters long!"
+        f" Who knows?{shrug_emoji}\n{color_white}5.{styling_end} {color_white}"
+        f"BE AWARE!{styling_end} There may be singular {color_white}OR"
+        f"{styling_end} plural words you may encounter.\n{color_white}6." 
+        f"{styling_end} Finally, insert your username and have fun!" 
+        f" {crazy_face}\n"
+        )
     print(rules)
 
 def input_username():
@@ -86,10 +86,9 @@ def input_username():
             break
         else:
             print(Fore.RED + 
-                f"""{prohibited} {username} is an INVALID username, please use 
-                one without numbers, special characters or spaces.""" +
+                f"{prohibited} {username} is an INVALID username, please use"
+                f" one without numbers, special characters or spaces.\n" +
                 Style.RESET_ALL)
-            # print(Style.RESET_ALL)
     return username
 
 def get_correct_word(list_of_words):
@@ -131,17 +130,18 @@ def guess_word():
         
             else:
                 lives_allowed = lives_allowed - 1
-                print(f"""Incorrect, You lost a life! You now have
-                    {lives_allowed} {heart_emoji} 's remaining.""")
+                print(
+                    f"Incorrect, You lost a life! You now have {lives_allowed}"
+                    f" {heart_emoji} 's remaining."
+                    )
 
         elif letter_guess in guessed_letters:
-            print(f"""{cross_emoji} Whoops! You cannot guess the same letter
-                twice. Please try again.""")
+            print(Fore.RED + f"{cross_emoji} Whoops! You cannot guess the same"
+                f" letter twice. Please try again." + Style.RESET_ALL)
 
         else:
-            print(Fore.RED + f"""{prohibited} INVALID character, please try
-                again.""" + Style.RESET_ALL)
-            # print(Style.RESET_ALL)
+            print(Fore.RED + f"{prohibited} INVALID character, please try"
+                f" again." + Style.RESET_ALL)
 
         #telling the user what letters they have guessed
         print("You have have used letters: ", ' '.join(guessed_letters))
@@ -153,31 +153,40 @@ def guess_word():
         print("")
 
     if lives_allowed == 0:
-        print(f"You Lost!{shocked_face}\nThe word was" + f" {new_word}")
+        print(
+            f"You {color_white}LOST!{styling_end}{shocked_face}\nThe word was"
+            + f" {color_white}{underline}{new_word}{styling_end}\n"
+            )
         play_again = input(
-            "Would you like to restart the game?" + " \033[1;32mY\033[0m"
-            + "/" + "\033[0;31mN\033[0m" + ": ").upper()
+            f"Would you like to start the game again? {color_green}Y"
+            f"{styling_end}/{color_red}N{styling_end}: "
+            ).upper()
         if play_again == "Y":
             print("Restarting game now...")
             #Code to restart game
         elif play_again == "N": 
-            print(f"Exiting Game Now...\nThank you for playing!{waving_hand}")
+            print(f"Exiting Game Now...\n\nThank you for playing!{waving_hand}")
         else:
-            print(f"""{confused_emoji} I didn't ask for that did I?, please 
-                enter Y or N.\n""")
+            print(
+                f"{confused_emoji} I didn't ask for that did I?, please enter"
+                f" Y or N.\n"
+                )
     else:
         print(f"Yay! You did it {face_with_hearts}")
         play_again = input(
-            "Would you like to restart the game?" + " \033[1;32mY\033[0m"
-            + "/" + "\033[0;31mN\033[0m" + ": ").upper()
+            f"Would you like to start the game? {color_green}Y{styling_end}/"
+            f"{color_red}N{styling_end}: "
+            ).upper()
         if play_again == "Y":
             print("Restarting game now...")
             #Code to restart game
         elif play_again == "N":
             print(f"Exiting Game Now...\nThank you for playing!{waving_hand}")
         else:
-            print(f"""{confused_emoji} I didn't ask for that did I?, please
-                enter Y or N.\n""")
+            print(
+                f"{confused_emoji} I didn't ask for that did I?, please enter"
+                f" Y or N.\n"
+                )
     return word_makeup
 
 def restart_game():
@@ -186,8 +195,6 @@ def restart_game():
     display rules function, when the user decides they want to play the game 
     again.
     """
-
-
 
 
 def main():
