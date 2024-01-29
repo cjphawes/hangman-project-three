aecab6997f121b36a01206fd4775195df1849cf4
cjphawes <cameronhawes98@gmail.com>
2024-01-29 14:34:49 +0000

    feat:create file for storing emoji vars

---
+++ emoji_dict.py
@@ -0,0 +1,13 @@
+import emoji
+
+#variables of all emoji's used in the hangman game
+waving_hand = emoji.emojize("\U0001F44B")
+confused_emoji = emoji.emojize("\U0001F928")
+heart_emoji = emoji.emojize("\u2764\ufe0f")
+heart_emoji_multiplied = heart_emoji * 8
+green_tick = emoji.emojize("\u2705")
+writing = emoji.emojize("\U0001F4DD")
+prohibited = emoji.emojize("\U0001F6AB")
+cross_emoji = emoji.emojize("\u274C")
+shocked_face = emoji.emojize("\U0001F626")
+face_with_hearts = emoji.emojize("\uE106")
