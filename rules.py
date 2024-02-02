b3b189f1138744ee1ac338118796135c7c392329
cjphawes <cameronhawes98@gmail.com>
2024-02-02 17:51:15 +0000

    refactor:remove whitespaces and correct line indentation

--- rules.py
+++ rules.py
@@ -1,21 +1,21 @@
 from emoji_dict import *
-from number_styling import * 
+from number_styling import *
 
 rules = (
-        f"\n{underline}Here are the rules{styling_end} {writing}\n\n"
-        f"{color_white}1.{styling_end} I will give you a mystery word, it is"
-        f" your job to guess the letters that make up that word.\n{color_white}"
-        f"2.{styling_end} You have 8 {heart_emoji_multiplied}  lives available,"
-        f" every time you get a letter incorrect, you will lose a {heart_emoji}"
-        f"\n{color_white}3.{styling_end} Guess a letter and I will tell you if"
-        f" it is correct or not.\n{color_white}4.{styling_end} Keep guessing"
-        f" until you either, lose all your {heart_emoji} 's, or you guess the"
-        f" word correctly {green_tick}\n{color_white}6.{styling_end} You will"
-        f" not know the length of the word until you make your first guess!"
-        f" Mysterious I know {winky_face}  It could be from 4-9 letters long!"
-        f" Who knows?{shrug_emoji}\n{color_white}5.{styling_end} {color_white}"
-        f"BE AWARE!{styling_end} There may be singular {color_white}OR"
-        f"{styling_end} plural words you may encounter.\n{color_white}6." 
-        f"{styling_end} Finally, insert your username and have fun!" 
-        f" {crazy_face}\n"
-        )
+    f"\n{underline}Here are the rules{styling_end} {writing}\n\n"
+    f"{color_white}1.{styling_end} I will give you a mystery word, it is"
+    f" your job to guess the letters that make up that word.\n{color_white}"
+    f"2.{styling_end} You have 8 {heart_emoji_multiplied}  lives available,"
+    f" every time you get a letter incorrect, you will lose a {heart_emoji}"
+    f"\n{color_white}3.{styling_end} Guess a letter and I will tell you if"
+    f" it is correct or not.\n{color_white}4.{styling_end} Keep guessing"
+    f" until you either, lose all your {heart_emoji} 's, or you guess the"
+    f" word correctly {green_tick}\n{color_white}6.{styling_end} You will"
+    f" not know the length of the word until you make your first guess!"
+    f" Mysterious I know {winky_face}  It could be from 4-9 letters long!"
+    f" Who knows?{shrug_emoji}\n{color_white}5.{styling_end} {color_white}"
+    f"BE AWARE!{styling_end} There may be singular {color_white}OR"
+    f"{styling_end} plural words you may encounter.\n{color_white}6."
+    f"{styling_end} Finally, insert your username and have fun!"
+    f" {crazy_face}\n"
+    )
